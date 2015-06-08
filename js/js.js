0735c17949e5d245889474865d307c209e63f027
AjeebKt <ajeebajb@gmail.com>
2015-06-08 16:52:20 +0530

    initial commit

---
+++ js/js.js
@@ -0,0 +1,8 @@
+function add ()
+		{
+			// var ul = document.getElementById("ul");
+			var name = document.getElementById("txt").value;
+			// var li = document.createElement("li");
+			document.getElementById("add").innerHTML = name;
+			appendChild(li);
+		}
